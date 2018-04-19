6633f9166356e1856d8ff79da26b6a56974d901f
yunqisong <yunqisong@foxmail.com>
2018-04-19 09:47:45 +0800

    更新连接池配置

--- src/main/java/com/ljwm/bootbase/config/MybatisPlusConfig.java
+++ src/main/java/com/ljwm/bootbase/config/MybatisPlusConfig.java
@@ -9,6 +9,7 @@ import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 import com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean;
 import com.baomidou.mybatisplus.spring.boot.starter.SpringBootVFS;
+import com.google.common.collect.Lists;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.apache.ibatis.mapping.DatabaseIdProvider;
@@ -28,6 +29,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.Collections;
 
 /**
  * JKhaled created by yunqisong@foxmail.com 2017/11/21
@@ -40,106 +42,107 @@ import java.sql.SQLException;
 @EqualsAndHashCode(callSuper = false)
 public class MybatisPlusConfig extends WebMvcConfigurerAdapter {
 
-    private String url;
+  private String url;
 
-    private String password;
+  private String password;
 
-    private String username;
+  private String username;
 
-    @Autowired(required = false)
-    private Interceptor[] interceptors;
+  @Autowired(required = false)
+  private Interceptor[] interceptors;
 
-    @Autowired
-    private MybatisProperties properties;
+  @Autowired
+  private MybatisProperties properties;
 
-    @Autowired(required = false)
-    private DatabaseIdProvider databaseIdProvider;
+  @Autowired(required = false)
+  private DatabaseIdProvider databaseIdProvider;
 
-    @Autowired
-    private ResourceLoader resourceLoader = new DefaultResourceLoader();
+  @Autowired
+  private ResourceLoader resourceLoader = new DefaultResourceLoader();
 
-    /**
-     * @Description : mybatis-plus SQL执行效率插件【生产环境可以关闭】
-     */
-    @Bean
-    public PerformanceInterceptor performanceInterceptor() {
-        return new PerformanceInterceptor();
+  /**
+   * @Description : mybatis-plus SQL执行效率插件【生产环境可以关闭】
+   */
+  @Bean
+  public PerformanceInterceptor performanceInterceptor() {
+    return new PerformanceInterceptor();
+  }
+
+  /**
+   * 配置DataSource
+   *
+   * @return
+   * @throws SQLException
+   */
+  @Bean
+  public DataSource dataSource() {
+    DruidDataSource dataSource = new DruidDataSource();
+    dataSource.setUrl(url);
+    dataSource.setUsername(username);//用户名
+    dataSource.setPassword(password);//密码
+    dataSource.setInitialSize(2);
+    dataSource.setMaxActive(20);
+    dataSource.setMinIdle(0);
+    dataSource.setMaxWait(60000);
+    dataSource.setValidationQuery("SELECT 1");
+    dataSource.setTestOnBorrow(false);
+    dataSource.setTestWhileIdle(true);
+    dataSource.setPoolPreparedStatements(false);
+    dataSource.setConnectionInitSqls(Lists.newArrayList("set names utf8mb4;"));
+    return dataSource;
+  }
+
+  /**
+   * @Description : mybatis-plus分页插件
+   * ---------------------------------
+   */
+  @Bean
+  public PaginationInterceptor paginationInterceptor() {
+    PaginationInterceptor page = new PaginationInterceptor();
+    page.setDialectType("mysql");
+    return page;
+  }
+
+  /**
+   * 这里全部使用mybatis-autoconfigure 已经自动加载的资源。不手动指定
+   * 配置文件和mybatis-boot的配置文件同步
+   *
+   * @return
+   */
+  @Bean
+  public MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean() {
+    MybatisSqlSessionFactoryBean mybatisPlus = new MybatisSqlSessionFactoryBean();
+    mybatisPlus.setDataSource(dataSource());
+    mybatisPlus.setVfs(SpringBootVFS.class);
+    if (StringUtils.hasText(this.properties.getConfigLocation())) {
+      mybatisPlus.setConfigLocation(this.resourceLoader.getResource(this.properties.getConfigLocation()));
     }
-
-    /**
-     * 配置DataSource
-     *
-     * @return
-     * @throws SQLException
-     */
-    @Bean
-    public DataSource dataSource() {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);//用户名
-        dataSource.setPassword(password);//密码
-        dataSource.setInitialSize(2);
-        dataSource.setMaxActive(20);
-        dataSource.setMinIdle(0);
-        dataSource.setMaxWait(60000);
-        dataSource.setValidationQuery("SELECT 1");
-        dataSource.setTestOnBorrow(false);
-        dataSource.setTestWhileIdle(true);
-        dataSource.setPoolPreparedStatements(false);
-        return dataSource;
+    mybatisPlus.setConfiguration(properties.getConfiguration());
+    if (!ObjectUtils.isEmpty(this.interceptors)) {
+      mybatisPlus.setPlugins(this.interceptors);
     }
-
-    /**
-     * @Description : mybatis-plus分页插件
-     * ---------------------------------
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor page = new PaginationInterceptor();
-        page.setDialectType("mysql");
-        return page;
+    // MP 全局配置，更多内容进入类看注释
+    GlobalConfiguration globalConfig = new GlobalConfiguration();
+    globalConfig.setDbType(DBType.MYSQL.name());
+    // ID 策略 AUTO->`0`("数据库ID自增") INPUT->`1`(用户输入ID") ID_WORKER->`2`("全局唯一ID") UUID->`3`("全局唯一ID")
+    globalConfig.setIdType(0);
+    mybatisPlus.setGlobalConfig(globalConfig);
+    MybatisConfiguration mc = new MybatisConfiguration();
+    mc.setDefaultScriptingLanguage(MybatisXMLLanguageDriver.class);
+    mybatisPlus.setConfiguration(mc);
+    if (this.databaseIdProvider != null) {
+      mybatisPlus.setDatabaseIdProvider(this.databaseIdProvider);
     }
-
-    /**
-     * 这里全部使用mybatis-autoconfigure 已经自动加载的资源。不手动指定
-     * 配置文件和mybatis-boot的配置文件同步
-     *
-     * @return
-     */
-    @Bean
-    public MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean() {
-        MybatisSqlSessionFactoryBean mybatisPlus = new MybatisSqlSessionFactoryBean();
-        mybatisPlus.setDataSource(dataSource());
-        mybatisPlus.setVfs(SpringBootVFS.class);
-        if (StringUtils.hasText(this.properties.getConfigLocation())) {
-            mybatisPlus.setConfigLocation(this.resourceLoader.getResource(this.properties.getConfigLocation()));
-        }
-        mybatisPlus.setConfiguration(properties.getConfiguration());
-        if (!ObjectUtils.isEmpty(this.interceptors)) {
-            mybatisPlus.setPlugins(this.interceptors);
-        }
-        // MP 全局配置，更多内容进入类看注释
-        GlobalConfiguration globalConfig = new GlobalConfiguration();
-        globalConfig.setDbType(DBType.MYSQL.name());
-        // ID 策略 AUTO->`0`("数据库ID自增") INPUT->`1`(用户输入ID") ID_WORKER->`2`("全局唯一ID") UUID->`3`("全局唯一ID")
-        globalConfig.setIdType(0);
-        mybatisPlus.setGlobalConfig(globalConfig);
-        MybatisConfiguration mc = new MybatisConfiguration();
-        mc.setDefaultScriptingLanguage(MybatisXMLLanguageDriver.class);
-        mybatisPlus.setConfiguration(mc);
-        if (this.databaseIdProvider != null) {
-            mybatisPlus.setDatabaseIdProvider(this.databaseIdProvider);
-        }
-        if (StringUtils.hasLength(this.properties.getTypeAliasesPackage())) {
-            mybatisPlus.setTypeAliasesPackage(this.properties.getTypeAliasesPackage());
-        }
-        if (StringUtils.hasLength(this.properties.getTypeHandlersPackage())) {
-            mybatisPlus.setTypeHandlersPackage(this.properties.getTypeHandlersPackage());
-        }
-        if (!ObjectUtils.isEmpty(this.properties.resolveMapperLocations())) {
-            mybatisPlus.setMapperLocations(this.properties.resolveMapperLocations());
-        }
-        return mybatisPlus;
+    if (StringUtils.hasLength(this.properties.getTypeAliasesPackage())) {
+      mybatisPlus.setTypeAliasesPackage(this.properties.getTypeAliasesPackage());
     }
+    if (StringUtils.hasLength(this.properties.getTypeHandlersPackage())) {
+      mybatisPlus.setTypeHandlersPackage(this.properties.getTypeHandlersPackage());
+    }
+    if (!ObjectUtils.isEmpty(this.properties.resolveMapperLocations())) {
+      mybatisPlus.setMapperLocations(this.properties.resolveMapperLocations());
+    }
+    return mybatisPlus;
+  }
 
 }
