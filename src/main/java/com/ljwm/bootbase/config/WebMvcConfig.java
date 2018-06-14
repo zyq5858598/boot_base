64e41c71935e9bcf0d26e3e5f8fa40d552c213a1
Yu Zhou <nuaazy@163.com>
2018-06-14 18:31:12 +0800

    register fastjson into extendMessageConverters

--- src/main/java/com/ljwm/bootbase/config/WebMvcConfig.java
+++ src/main/java/com/ljwm/bootbase/config/WebMvcConfig.java
@@ -1,24 +1,29 @@
 package com.ljwm.bootbase.config;
 
-import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.ljwm.bootbase.interceptor.ApiLogInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by yuzhou on 2018/3/14.
  */
+@Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+  @Value("${spring.http.converters.preferred-json-mapper}")
+  private String [] jsonMappers;
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
@@ -27,7 +32,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
   }
 
   @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {}
+
+  private void addFastJsonConverter(List<HttpMessageConverter<?>> converters) {
+    // TODO: 不是简单移除掉Jackson converter，而是排在其它json lib的前面
+    converters.removeIf(converter -> converter instanceof MappingJackson2HttpMessageConverter);
+
+    log.info("Add a FastJsonHttpMessageConverter as HttpMessageConverter");
     //定义一个转换消息的对象
     FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
     //添加fastjson的配置信息 比如 ：是否要格式化返回的json数据
@@ -44,5 +55,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     fastConverter.setFastJsonConfig(fastJsonConfig);
     //将转换器添加到converters中
     converters.add(fastConverter);
+  }
+
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    if (Arrays.stream(jsonMappers).anyMatch(e -> e.equalsIgnoreCase("fastjson"))) {
+      this.addFastJsonConverter(converters);
+    }
+
+    log.debug("There has been {} HttpMessageConverters when extend", converters.size());
+
+    converters.forEach(converter -> {
+      log.debug("HttpMessageConverter: {}", converter.getClass().getName());
+    });
   }
 }
